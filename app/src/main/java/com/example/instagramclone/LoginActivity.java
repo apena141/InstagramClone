5c696bc74637a71ea77ef6fae5ae1c75fd1b51b5
apena141 <apena141@fiu.edu>
2021-02-21 12:14:54 -0500

    Init commit

---
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUserName);
+        etPassword = findViewById(R.id.etPassword);
+        btLogin = findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(userName, password);
+            }
+        });
+    }
+
+    private void loginUser(String userName, String password) {
+        Log.i(TAG, "Trying to login user: " + userName);
+    }
+}
