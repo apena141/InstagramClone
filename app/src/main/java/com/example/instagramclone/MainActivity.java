bc8e06adb191e4a463f38c0501e8129d894e918c
apena141 <apena141@fiu.edu>
2021-02-21 16:15:25 -0500

    Defines Data object and Query Posts

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btTakePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btTakePicture = findViewById(R.id.btTakePicture);
+
+        queryPosts();
+    }
+
+    private void queryPosts(){
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting objects ", e);
+                }
+                for(Post post: objects){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,12 +3,17 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Registers parse model
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("JVu19Q8IwhR1wB9j3MWUywPWiRwO0mu6INcAyYC0")
                 .clientKey("uGloTJWVMkfEr22L6Mrqh5haA9StBTLlGdhtiPDF")
